32e6ec3685f9f34982123f3c40618be2db00d81f
Хассен Джевер <osmanova@simferopol.softlab.ru>
2026-05-20 17:35:01 +0300

    chore(app): Add vitest for writing unit-test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import react from '@vitejs/plugin-react';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -29,5 +29,9 @@ export default defineConfig({
     host: true,
     port: 5173,
     strictPort: true,
+  },
+  test: {
+    environment: 'jsdom',
+    setupFiles: './src/shared/config/test/setupTests.ts',
   },
 });
